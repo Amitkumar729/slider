481822cc3d3e0ebf2047bd59c0e07815f9c449c5
Amit kumar <ak0638010@gmail.com>
2022-09-04 12:42:46 +0530

    final submit

--- src/App.js
+++ src/App.js
@@ -2,32 +2,31 @@ import ImageSlider from "./ImageSlider";
 
 function App() {
   const slides = [
-    { url: "http://localhost:3000/Anime1.jpg", title: "op" },
-    { url: "http://localhost:3000/Anime2.jpg", title: "beach" },
-    { url: "http://localhost:3000/Anime3.jpg", title: "ocean"},
-    { url: "http://localhost:3000/Anime4.jpg", title: "jjk" },
-    { url: "http://localhost:3000/Anime5.jpg", title: "itadori" },
-    { url: "http://localhost:3000/Anime6.jpg", title: "sukuna" },
-    { url: "http://localhost:3000/Anime7.jpg", title: "gojo" },
-    { url: "http://localhost:3000/Anime8.jpg", title: "luffy" },
-    { url: "http://localhost:3000/Anime9.jpg", title: "zoro" },
-    { url: "http://localhost:3000/Anime10.jpg", title: "nami" },
+    { url: "http://localhost:3000/Anime1.jpg.jpg", title: "op" },
+    { url: "http://localhost:3000/Anime2.jpg.jpg", title: "beach" },
+    { url: "http://localhost:3000/Anime3.jpg.jpg", title: "ocean" },
+    { url: "http://localhost:3000/Anime4.jpg.jpg", title: "jjk" },
+    { url: "http://localhost:3000/Anime7.jpg.jpg", title: "gojo" },
+    { url: "http://localhost:3000/Anime8.jpg.jpg", title: "luffy" },
+    // { url: "http://localhost:3000/Anime9.jpg.jpg", title: "zoro" },
+    { url: "http://localhost:3000/Anime10.jpg.jpg", title: "nami" },
+    { url: "http://localhost:3000/Anime11.jpg.jpg", title: "sanzi" },
+    { url: "http://localhost:3000/Anime12.jpg.jpg", title: "choper" },
   ];
 
-const containerStyles = {
-  width: "700px",
-  height: "350px",
-  margin: "0 auto",
-};
+  const containerStyles = {
+    width: "700px",
+    height: "350px",
+    margin: "0 auto",
+  };
 
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>Anime</h1>
 
-      <div style={containerStyles} >
+      <div style={containerStyles}>
         <ImageSlider slides={slides} />
       </div>
-
     </div>
   );
 }
